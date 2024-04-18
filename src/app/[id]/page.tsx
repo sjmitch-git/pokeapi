54af18883d6e8c5c1d13cf44e76bc677a0a5dac5
sjmitch-git <sjmitch20@outlook.com>
2024-04-18 18:43:11 +0100

    Adds fetch data

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,10 +1,24 @@
 import { Title } from '@/components'
 
-export default function Detail({ params }: { params: { id: string } }) {
-	console.log('id', params)
+export default async function Detail({ params }: { params: { id: string } }) {
+	const data = await getData(params.id)
+	console.log(data)
 	return (
 		<div>
-			<Title title={params.id} />
+			<Title title={data.name} />
 		</div>
 	)
+}
+
+async function getData(id: string) {
+	const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+	// The return value is *not* serialized
+	// You can return Date, Map, Set, etc.
+
+	if (!res.ok) {
+		// This will activate the closest `error.js` Error Boundary
+		throw new Error('Failed to fetch data')
+	}
+
+	return res.json()
 }
