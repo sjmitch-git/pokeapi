d7c7456e866cd78069694a760c39fa36618b5db8
sjmitch-git <sjmitch20@outlook.com>
2024-04-22 18:27:59 +0100

    Fixes getId util type error

--- src/utils/getId.ts
+++ src/utils/getId.ts
@@ -1,6 +1,6 @@
-function getId(url: string): string | null {
+function getId(url: string): string {
 	const match = url.match(/(\d+)(?!.*\d)/)
-	return match ? match[0] : null
+	return match ? match[0] : '1'
 }
 
 export default getId
