c16ee9156e91d3d7cb69a273c2a2bb0182164677
sjmitch-git <sjmitch20@outlook.com>
2024-09-22 12:41:32 +0100

    SEO fixes

--- src/app/list/[id]/page.tsx
+++ src/app/list/[id]/page.tsx
@@ -25,7 +25,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 	return {
 		title: title,
-		description: `${description} #${data.name} #Pokemon`,
+		description: `${description}`,
 		alternates: {
 			canonical: `list/${id}`,
 		},
